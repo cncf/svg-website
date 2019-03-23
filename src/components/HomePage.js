42bdaf9bbf2d63a8154b15f5f84a7407c062ca12
ZeusTheTrueGod <l-2732@yandex.ru>
2019-03-23 07:36:55 +0000

    adding an example dropzone

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { pure } from 'recompose';
 import Header from './Header';
+import Dropzone from 'react-dropzone'
 
 
 
 const HomePage = ({}) => {
   return [
     <Header />,
-    <div>
-      <div><span>Status: </span></div>
-      <div><span>Placeholder for a file input</span></div>
-      <div><span>Placeholder for an original SVG</span></div>
-      <div><span>Placeholder for an output SVG</span></div>
-      <div><button>Clear</button></div>
-      <div><button>Download></button></div>
-    </div>
+    <Dropzone onDrop={ (x) => console.info(x) }>
+      {({getRootProps, getInputProps}) => (
+        <div {...getRootProps()}>
+          <input {...getInputProps() } />
+          <div>Drag 'n' drop some files here, or click to select files</div>
+          <div><span>Status: </span></div>
+          <div><span>Placeholder for a file input</span></div>
+          <div><span>Placeholder for an original SVG</span></div>
+          <div><span>Placeholder for an output SVG</span></div>
+          <div><button>Clear</button></div>
+          <div><button>Download></button></div>
+        </div>
+      )}
+    </Dropzone>
   ]
 };
 
