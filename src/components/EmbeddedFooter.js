332dc1a407ca98c06bd8416855b1d204ba757725
ZeusTheTrueGod <l-2732@yandex.ru>
2019-04-04 15:22:58 +0100

    remove non used files

--- src/components/EmbeddedFooter.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { pure } from 'recompose';
-const EmbeddedFooter = () => {
-  const originalLink = window.location.pathname.replace('&embed=yes', '').replace('&embed=true', '');
-  return <h1 style={{ marginTop: 20, width: '100%', textAlign: 'center' }}>
-    <a target="_blank" href={originalLink}>View</a> the full interactive landscape
-  </h1>
-}
-export default pure(EmbeddedFooter);
